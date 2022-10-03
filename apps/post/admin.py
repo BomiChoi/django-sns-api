65f35e4fa0e10345652ac923e5383e71e55bfcd5
Bomi Choi <hibomi97@gmail.com>
2022-10-03 14:08:46 +0900

    Feat: Post, Hashtag 모델 생성
    - Post, Hashtag 모델 작성
    - PostAdmin, HashtagAdmin 작성

--- apps/post/admin.py
+++ apps/post/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from .models import Post, Hashtag
+
+
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'content',
+        'user',
+        'created_at',
+        'updated_at',
+        'hits'
+    )
+
+
+@admin.register(Hashtag)
+class HashtagAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name'
+    )
--- apps/post/models.py
+++ apps/post/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from apps.common.models import TimeStampedModel
+from apps.user.models import User
+
+
+class Post(TimeStampedModel):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    hashtags = models.ManyToManyField('Hashtag', related_name='products', blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    hits = models.PositiveIntegerField(default=0)
+
+
+class Hashtag(models.Model):
+    name = models.CharField(max_length=60, unique=True)
