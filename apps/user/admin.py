7bf0556530ee7ffb9e71f3970ab6255217ed80e6
Bomi Choi <hibomi97@gmail.com>
2022-10-03 12:45:01 +0900

    Feat: User 모델 생성
    - BaseUserModel 상속받아 User 모델 작성
    - BaseUserManager 상속받아 UserManager 작성
    - UserAdmin 상속받아 CustomUserAdmin 작성
      - UserCreationForm, UserChangeForm 작성

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,104 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
-# Register your models here.
+from .models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password Confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def clean_password2(self):
+        """ 비밀번호가 일치하는지 검사"""
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("비밀번호가 일치하지 않습니다.")
+        return password2
+
+    def save(self, commit=True):
+        """ 유저 생성 후 비밀번호 설정 """
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'password',
+            'name',
+            'gender',
+            'age',
+            'phone',
+            'is_staff',
+            'is_superuser',
+            'is_active',
+        )
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'email',
+                'password',
+            )
+        }),
+        ('Personal Info', {
+            'fields': (
+                'name',
+                'gender',
+                'age',
+                'phone',
+            )
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_staff',
+                'is_superuser',
+            )
+        })
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+            )
+        }),
+    )
+
+    list_display = (
+        'id',
+        'email',
+        'name',
+        'gender',
+        'age',
+        'phone',
+        'is_staff',
+        'is_superuser',
+        'is_active',
+        'created_at',
+    )
+
+    ordering = ('email',)
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('must have email')
+        if not password:
+            raise ValueError('must have password')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_user(self, email, password, **extra_fields):
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields['is_staff'] = True
+        extra_fields['is_superuser'] = True
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=30, null=True, blank=True)
+    gender = models.CharField(max_length=4, null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    phone = models.CharField(max_length=13, null=True, blank=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
